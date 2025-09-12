6307b36da92cffabce3a044b5ccfae135f902389
takascembericom <takascembericom@gmail.com>
2025-09-12 10:37:00 +0000

    fix: proper vite.config with sourcemap

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,7 +9,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
       '@shared': path.resolve(__dirname, '../shared'),
     },
-  ,build: { sourcemap: true }
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+  },
+  build: {
+    sourcemap: true,
   },
 })
